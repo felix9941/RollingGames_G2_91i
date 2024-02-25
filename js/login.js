ce830756a8dc344df8090e6a6449230909ee5f6d
Jose Martin Fe <149723252+Jose-Martin-Fe@users.noreply.github.com>
2024-02-25 18:09:25 -0300

    Corrección de generación ID

--- js/login.js
+++ js/login.js
@@ -105,10 +105,11 @@ navbarLogin.innerHTML = `
 </div>`;
 
 (() => {
-  const generarID = () => {
-    const usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
-    return usuarios[usuarios.length - 1].id + 1;
-  };
+const generarID = () => {
+  const usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
+  return usuarios.length > 0 ? usuarios[usuarios.length - 1].id + 1 : 1;
+};
+
 
   const newUser = {
     id: generarID(),
