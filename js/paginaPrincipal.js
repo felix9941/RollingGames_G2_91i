8bfaeb0361a0ee2ce4560353ad3ede6e4f96df82
Jose Martin Fe <martin.fesito@gmail.com>
2024-02-29 09:02:42 -0300

    Correcciones de proyecto

--- js/paginaPrincipal.js
+++ js/paginaPrincipal.js
@@ -458,10 +458,6 @@ const catalogoJuegos = [
     url: "https://youtu.be/z7f4paq1Fvg",
   },
 ];
-/*
-const juegoDestacado =
-  catalogoJuegos[Math.floor(Math.random() * catalogoJuegos.length)];
-*/
 
 const navbarRegistro = document.getElementById("navbar-registro");
 navbarRegistro.innerHTML = `
@@ -534,6 +530,58 @@ juegosDestacados.innerHTML = `
   </div>
 </div>
 `;
+
+const footerGeneral = document.getElementById("footerGeneral");
+footerGeneral.innerHTML = ` <div class="col-12 col-md-6 col-lg-4 d-flex justify-content-center">
+<a href="paginaPrincipal.html"
+  ><img
+    src="../img/Logo Play Gaming.png"
+    alt="Logo-PlayGaming"
+    width="150vw"
+    class="pb-3"
+/></a>
+</div>
+
+<div class="col-12 col-md-6 col-lg-4 d-flex justify-content-center">
+<div class="redes mt-3">
+  <div class="Redes2 d-flex justify-content-center icons">
+    <a href="error404.html"
+      ><i class="fa-brands fa-square-facebook fa-3x mx-2"></i
+    ></a>
+    <a href="error404.html"
+      ><i class="fa-brands fa-square-x-twitter fa-3x"></i
+    ></a>
+    <a href="error404.html"
+      ><i class="fa-brands fa-square-instagram fa-3x mx-2"></i
+    ></a>
+    <a href="error404.html"
+      ><i class="fa-brands fa-square-whatsapp fa-3x"></i
+    ></a>
+  </div>
+
+  <h5 class="text-center mt-3">
+    &copy; 2024 RollingsGame. Todos los derechos reservados.
+  </h5>
+</div>
+</div>
+
+<div class="col-12 col-md-6 col-lg-4 d-flex justify-content-center">
+<div class="informacion-pag">
+  <div
+    class="informacion-pag text-center mt-4 mb-3 listNone"
+    
+  >
+    <li><a href="paginaPrincipal.html">Inicio</a></li>
+    <li><a href="sobreNosotros.html">Sobre nosotros</a></li>
+    <li><a href="contacto.html">Contacto</a></li>
+    <li>
+      <a href="error404.html"
+        >Politicas de privacidad</a
+      >
+    </li>
+  </div>
+</div>
+</div>`;
 
 function mostrarJuego(id) {
   const juego = catalogoJuegos.find((juego) => juego.id === id);
