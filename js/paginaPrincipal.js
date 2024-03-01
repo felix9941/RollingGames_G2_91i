b2d22bc972789a45eb9bc0e93bb1b27a2cbc59cd
Jose Martin Fe <martin.fesito@gmail.com>
2024-03-01 17:29:23 -0300

    Correcion de localStorage categoria

--- js/paginaPrincipal.js
+++ js/paginaPrincipal.js
@@ -203,7 +203,7 @@ filtroJuegos.innerHTML = `
 `;
 const catalogoJuegosJSON = JSON.stringify(catalogoJuegos);
 
-localStorage.setItem("catalogoJuegos", catalogoJuegosJSON);
+localStorage.setItem("catalogoDeJuegos", catalogoJuegosJSON);
 
 const tarjetasCarrousel = document.getElementById("tarjetasCarrousel");
 tarjetasCarrousel.innerHTML = `
