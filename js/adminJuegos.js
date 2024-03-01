809d96a9ed9cb7858be1356d1eb3a0f1905a11e5
Isas321 <javier.isas321@gmail.com>
2024-03-01 02:54:48 -0300

    creacion de boton favorito

--- js/adminJuegos.js
+++ js/adminJuegos.js
@@ -4,13 +4,31 @@ const juegos = JSON.parse(localStorage.getItem("catalogoDeJuegos")) || [];
 
 console.log(juegos);
 
+function toggleCheck(event) {
+  event.preventDefault();
+  var check = document.getElementById("check");
+  var starIcon = document.getElementById("starIcon");
+
+  if (check.classList.contains("checked")) {
+    check.classList.remove("checked");
+    check.classList.add("unchecked");
+    starIcon.classList.remove("text-warning");
+    starIcon.classList.add("text-secondary");
+  } else {
+    check.classList.remove("unchecked");
+    check.classList.add("checked");
+    starIcon.classList.remove("text-secondary");
+    starIcon.classList.add("text-warning");
+  }
+}
+
 const nuevoJuego = document.getElementById("idBotonNuevoJuego");
 nuevoJuego.innerHTML = `  
-<div class="row">
-  <div class="col-12 d-flex justify-content-end mt-5">  
+
+  <div class="">  
       <!-- Button trigger modal -->
       <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#exampleModal">
-        Nuevo Juego
+       + Nuevo Juego
       </button>
       
       <!-- Modal -->
@@ -40,9 +58,9 @@ nuevoJuego.innerHTML = `
                 </div>
 
                 <div class="mb-3">
-                  <label for="exampleInputPassword1" class="form-label">Descripcion</label>
-                  <input type="text" class="form-control" id="idInputRol" name="rol" value="">
-                </div>
+                <label for="exampleFormControlTextarea1" class="form-label">Descripcion</label>
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>   
+              </div>
 
                 <div class="mb-3">
                   <label for="disabledSelect" class="form-label">Publicar</label>
@@ -64,6 +82,13 @@ nuevoJuego.innerHTML = `
         </div>
       </div>
   </div>
+
+  
+
+<div class="btn-group" role="group" aria-label="Basic example">
+  <button type="button" class="btn btn-primary"><i class="fas fa-star fa-2x text-secondary ms-4"></i></button>
+  <button type="button" class="btn btn-primary">Middle</button>
+  <button type="button" class="btn btn-primary">Right</button>
 </div>
 `;
 /* Tablas de juego */
@@ -83,11 +108,7 @@ tablaJuegos.innerHTML = juegos
     </div>
   </td>
   <td>
-    <div class="container">
-      <div class="row">
-        <div class="col-sm-4">
-
-
+    <div class="container d-flex">
           <!-- Button trigger modal -->
           <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#exampleModal-${
             juego.id
@@ -96,80 +117,81 @@ tablaJuegos.innerHTML = juegos
           </button>
           
           <!-- Modal -->
-          <div class="modal  fade" id="exampleModal-${
-            juego.id
-          }" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-              <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title w-100 text-center">Editar Juego: ${
-                      juego.titulo
-                    }</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-
-                <div class="modal-body">
-                  <form>
-                    <div class="mb-3">
-                      <label for="exampleInputEmail1" class="form-label">Nombre</label>
-                      <input type="text" class="form-control" id="idInputUser" name="user" value="${
+            <div class="modal  fade" id="exampleModal-${
+              juego.id
+            }" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+              <div class="modal-dialog">
+                <div class="modal-content">
+                  <div class="modal-header">
+                      <h5 class="modal-title w-100 text-center">Editar Juego: ${
                         juego.titulo
-                      }" aria-describedby="emailHelp" >
-                    </div>
-                    
-                    <div class="mb-3">
-                    <label for="categoriaSelect" class="form-label">Categoria</label>
-                    <select id="categoriaSelect" class="form-select">
-                      <option value="">Seleccionar categoria</option>
-                      <option value="Aventura" ${
-                        juego.categoria === "Aventura" ? "selected" : ""
-                      }>Aventura</option>
-                      <option value="Acción" ${
-                        juego.categoria === "Acción" ? "selected" : ""
-                      }>Acción</option>
-                      <option value="Estrategia" ${
-                        juego.categoria === "Estrategia" ? "selected" : ""
-                      }>Estrategia</option>
-                      <option value="Deportes" ${
-                        juego.categoria === "Deportes" ? "selected" : ""
-                      }>Deportes</option>
-                    </select>
+                      }</h5>
+                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                   </div>
+
+                  <div class="modal-body">
+                    <form>
+                      <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Nombre</label>
+                        <input type="text" class="form-control" id="idInputUser" name="user" value="${
+                          juego.titulo
+                        }" aria-describedby="emailHelp" >
+                      </div>
+                    
+                      <div class="mb-3">
+                        <label for="categoriaSelect" class="form-label">Categoria</label>
+                        <select id="categoriaSelect" class="form-select">
+                          <option value="">Seleccionar categoria</option>
+                          <option value="Aventura" ${
+                            juego.categoria === "Aventura" ? "selected" : ""
+                          }>Aventura</option>
+                          <option value="Acción" ${
+                            juego.categoria === "Acción" ? "selected" : ""
+                          }>Acción</option>
+                          <option value="Estrategia" ${
+                            juego.categoria === "Estrategia" ? "selected" : ""
+                          }>Estrategia</option>
+                          <option value="Deportes" ${
+                            juego.categoria === "Deportes" ? "selected" : ""
+                          }>Deportes</option>
+                        </select>
+                      </div>
     
-                    <div class="mb-3">
-                      <label for="exampleInputPassword1" class="form-label">Descripcion</label>
-                      <input type="text" class="form-control" id="idInputRol" name="rol" value="">
-                    </div>
+                      <div class="mb-3">
+                        <label for="exampleFormControlTextarea1" class="form-label">Descripcion</label>
+                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3">${
+                          juego.descripcion
+                        }</textarea>   
+                      </div>
     
-                    <div class="mb-3">
-                      <label for="disabledSelect" class="form-label">Publicar</label>
-                      <select id="disabledSelect" class="form-select">
-                        <option value="Si" selected>Si</option>
-                        <option value="No">No</option>
-                      </select>
-                    </div>
-    
-                    <div class="mb-3">
-                      <label for="exampleInputPassword1" class="form-label">Link trailer de juego</label>
-                      <input type="text" class="form-control" id="idInputRol" name="rol" value="">
-                    </div>
-    
-                    <button type="submit" class="btn btn-primary" id="idBotonGuardar">Guardar Cambios</button>
-                  </form>
+                      <div class="mb-3">
+                        <label for="disabledSelect" class="form-label">Publicar</label>
+                        <select id="disabledSelect" class="form-select">
+                          <option value="Si" selected>Si</option>
+                          <option value="No">No</option>
+                        </select>
+                      </div>
+      
+                      <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label">Link trailer de juego</label>
+                        <input type="text" class="form-control" id="idInputRol" name="rol" value="${
+                          juego.url
+                        }">
+                      </div>
+      
+                      <button type="submit" class="btn btn-primary" id="idBotonGuardar">Guardar Cambios</button>
+                    </form>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        <div class="col-sm-6">
-        <style>
-        .text-yellow {
-            color: yellow;
-        }
-      </style>
-        <i class="fas fa-star fa-2x text-secondary ms-4"></i>
-      </div>
+
+
+          <i class="fas fa-star fa-2x text-secondary ms-4"></i>
+
     </div>
   </td> 
-</tr>`
+</tr>
+`
   )
   .join("");
