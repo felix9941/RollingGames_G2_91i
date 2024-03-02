7f55713f407be62d26f1fb7f0c0edd874dc215ec
Felix Figueroa <felixfigueroa41@gmail.com>
2024-03-01 23:44:28 -0300

    Se continua con la logica de carrito en su JS

--- js/carrito.js
+++ js/carrito.js
@@ -1,5 +1,5 @@
-const navbarRegistro = document.getElementById("navbar-registro");
-navbarRegistro.innerHTML = `
+const navbarUsuario = document.getElementById("navbar-usuario");
+navbarUsuario.innerHTML = `
 <div class="container-fluid">
 <a href="../index.html" class="d-flex align-items-center enlace-logo">
   <img class="ms-1 px-2" src="../img/Logo Play Gaming.png" alt="Logo Play Gamming" />
@@ -21,8 +21,7 @@ navbarRegistro.innerHTML = `
     <li class="nav-item">
       <a
         class="nav-link btn-hover btn-focus text-navbar anta-regular ms-3 px-2"
-        href="../page/sobreNosotros.html"
-        >Sobre nosotros</a
+        href="../page/sobreNosotros.html">Sobre nosotros</a
       >
     </li>
     <li class="nav-item">
@@ -33,19 +32,125 @@ navbarRegistro.innerHTML = `
       >
     </li>
     <li class="nav-item">
-      <a
-        class="nav-link btn-hover btn-focus text-navbar anta-regular ms-3 px-2"
-        href="../page/login.html"
-        >Iniciar sesion</a
-      >
-    </li>
-    <li class="nav-item">
-      <a
-        class="nav-link btn-hover btn-focus text-navbar anta-regular ms-3 px-2"
-        href="../page/registro.html"
-        >Registrarse</a
-      >
-    </li>
+        <div class="dropdown">
+           <a class="dropdown-toggle nav-link btn-hover btn-focus text-navbar anta-regular ms-3 px-2" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+           Mi cuenta
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="favoritos.html">Favoritos</a></li>
+            <li><a class="dropdown-item" href="carrito.html">Carrito</a></li>
+            <li><a class="dropdown-item" href="error404.html"">Mis datos</a></li>
+            <li><a class="dropdown-item" href="#" onclick="cerrarSesion()">Cerrar Sesion</a></li>
+          </ul>
+        </div>
+      </li>
   </ul>
 </div>
 </div>`;
+const footerGeneral = document.getElementById("footerGeneral");
+footerGeneral.innerHTML = ` <div class="col-12 col-md-6 col-lg-4 d-flex justify-content-center">
+<a href="paginaPrincipal.html"
+  ><img
+    src="../img/Logo Play Gaming.png"
+    alt="Logo-PlayGaming"
+    width="150vw"
+    class="pb-3"
+/></a>
+</div>
+
+<div class="col-12 col-md-6 col-lg-4 d-flex justify-content-center">
+<div class="redes mt-3">
+  <div class="Redes2 d-flex justify-content-center icons">
+    <a href="error404.html"
+      ><i class="fa-brands fa-square-facebook fa-3x mx-2"></i
+    ></a>
+    <a href="error404.html"
+      ><i class="fa-brands fa-square-x-twitter fa-3x"></i
+    ></a>
+    <a href="error404.html"
+      ><i class="fa-brands fa-square-instagram fa-3x mx-2"></i
+    ></a>
+    <a href="error404.html"
+      ><i class="fa-brands fa-square-whatsapp fa-3x"></i
+    ></a>
+  </div>
+
+  <h5 class="text-center mt-3">
+    &copy; 2024 RollingsGame. Todos los derechos reservados.
+  </h5>
+</div>
+</div>
+
+<div class="col-12 col-md-6 col-lg-4 d-flex justify-content-center">
+<div class="informacion-pag">
+  <div
+    class="informacion-pag text-center mt-4 mb-3 listNone"
+    
+  >
+    <li><a href="paginaPrincipal.html">Inicio</a></li>
+    <li><a href="sobreNosotros.html">Sobre nosotros</a></li>
+    <li><a href="contacto.html">Contacto</a></li>
+    <li>
+      <a href="error404.html"
+        >Politicas de privacidad</a
+      >
+    </li>
+  </div>
+</div>
+</div>`;
+
+const carritoJuegos = document.getElementById("carritoJuegos");
+
+const usersList = JSON.parse(localStorage.getItem("usuarios")) || [];
+const userCarrito = usersList.find((u) => u.carrito.length && u.login === true);
+
+if (userCarrito) {
+  if (userCarrito.length) {
+    carritoJuegos.innerHTML = userCarrito.juego.map(
+      (juego) => `
+    <div class="row">
+        <tbody>       
+          <tr>
+            <th scope="row">${juego.id}</th>
+            <td>${juego.imagen}</td>
+            <td>${juego.titulo}</td>
+            <td>${juego.precio}</td>
+            <td>
+              <button type="button" class="btn btn-danger" onclick="eliminarJuego">Eliminar</button>
+            </td>
+          </tr> 
+        </tbody>
+        <tbody>
+        <tr id="filaTotal">
+          <th colspan="3">Total($)</th>
+          <td id="total" colspan="1">0</td>
+        </tr>
+      </tbody>
+      </table>
+      <div style="text-align: right;">
+        <a href="../page/error404.html" class="pagar anta-regular">Pagar</a>
+      </div>
+    </div>
+`
+    );
+  } else {
+    userCarrito.innerHTML =
+      "<h3 class= 'text-center my-5'>No hay nada en Carrito</h3>";
+  }
+}
+
+function cerrarSesion() {
+  const usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
+
+  const userLogin = usuarios.find((u) => u.id && u.login === true);
+
+  if (userLogin) {
+    userLogin.login = false;
+
+    localStorage.setItem("usuarios", JSON.stringify(usuarios));
+  }
+
+  setTimeout(() => {
+    window.location.href = "/index.html";
+  }, 1000);
+}
