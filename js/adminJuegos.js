9abb6e0891187023575df730eea8fe5c7b238df2
Isas321 <javier.isas321@gmail.com>
2024-03-04 02:54:00 -0300

    analisis entre destacado y eliminacion fisica

--- js/adminJuegos.js
+++ js/adminJuegos.js
@@ -1,3 +1,165 @@
+/* const usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
+const usuario = usuarios.filter((usu) => usu.login === true);
+console.log(usuario.length);
+if (usuario.length === 1) { */
+//Esta logueado
+
+//Navbar
+const navbarAdminUsuarios = document.getElementById("navbar-admin");
+navbarAdminUsuarios.innerHTML = `<a href="paginaPrincipal.html" class="d-flex align-items-center enlace-logo">
+  <img class="ms-1 px-2" src="../img/Logo Play Gaming.png" alt="logo de la empresa"/>
+  </a>
+  <button
+  class="navbar-toggler"
+  type="button"
+  data-bs-toggle="collapse"
+  data-bs-target="#navbarSupportedContent"
+  aria-controls="navbarSupportedContent"
+  aria-expanded="false"
+  aria-label="Toggle navigation"
+  >
+  <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+  <ul class="navbar-nav ms-auto me-5 mb-2 mb-lg-0">
+    <li class="nav-item"></li>
+  
+    <li class="nav-item">
+      <a
+        class="nav-link btn-hover btn-focus text-navbar anta-regular ms-3 px-2"
+        href="sobreNosotros.html"
+        >Sobre nosotros</a
+      >
+    </li>
+    <li class="nav-item">
+      <a
+        class="nav-link btn-hover btn-focus text-navbar anta-regular ms-3 px-2"
+        href="contacto.html"
+        >Contacto</a
+      >
+    </li>
+    <li class="nav-item">
+      <a
+        class="nav-link btn-hover btn-focus text-navbar anta-regular ms-3 px-2"
+        href="login.html" id="loginItem">Iniciar sesion</a
+      >
+    </li>
+    <li class="nav-item">
+      <a
+        class="nav-link btn-hover btn-focus text-navbar anta-regular ms-3 px-2"
+        href="registro.html" id="registerItem">Registrarse</a
+      >
+    </li>
+    <li class="nav-item" id="administracion">
+          <div class="dropdown">
+             <a class="dropdown-toggle nav-link btn-hover btn-focus text-navbar anta-regular ms-3 px-2" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+             Administracion
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="adminUsuarios.html">Admin Usuarios</a></li>
+              <li><a class="dropdown-item" href="adminProductos.html">Admin Productos</a></li>
+            </ul>
+          </div>
+        </li>
+    <li class="nav-item" id="micuenta">
+    <div class="dropdown">
+       <a class="dropdown-toggle nav-link btn-hover btn-focus text-navbar anta-regular ms-3 px-2" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+       Mi cuenta
+      </a>
+      <ul class="dropdown-menu">
+        <li><a class="dropdown-item" href="favoritos.html">Favoritos</a></li>
+        <li><a class="dropdown-item" href="carrito.html">Carrito</a></li>
+        <li><a class="dropdown-item" href="error404.html"">Mis datos</a></li>
+        <li><a class="dropdown-item" href="#" onclick="cerrarSesion()">Cerrar Sesion</a></li>
+      </ul>
+    </div>
+  </li>
+  </ul>
+  </div>`;
+
+(() => {
+  const usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
+  const botonInicio = document.getElementById("loginItem");
+  const botonRegistro = document.getElementById("registerItem");
+  const botonLogin = document.getElementById("micuenta");
+  const botonLoginAdmin = document.getElementById("administracion");
+
+  const userLogin = usuarios.find(
+    (u) => u.login === true && u.rol === "usuario"
+  );
+  const userLoginAdmin = usuarios.find(
+    (u) => u.login === true && u.rol === "admin"
+  );
+
+  if (userLoginAdmin) {
+    botonInicio.classList.add("d-none");
+    botonRegistro.classList.add("d-none");
+    botonLoginAdmin.classList.add("d-block");
+  } else if (userLogin) {
+    botonInicio.classList.add("d-none");
+    botonRegistro.classList.add("d-none");
+    botonLoginAdmin.classList.add("d-none");
+  } else {
+    botonInicio.classList.add("d-block");
+    botonRegistro.classList.add("d-block");
+    botonLogin.classList.add("d-none");
+    botonLoginAdmin.classList.add("d-none");
+  }
+})();
+
+// Footer general
+const footerGeneral = document.getElementById("footerGeneral");
+footerGeneral.innerHTML = ` <div class="col-12 col-md-6 col-lg-4 d-flex justify-content-center">
+<a href="./paginaPrincipal.html"
+  ><img
+    src="../img/Logo Play Gaming.png"
+    alt="Logo-PlayGaming"
+    width="150vw"
+    class="pb-3"
+/></a>
+</div>
+
+<div class="col-12 col-md-6 col-lg-4 d-flex justify-content-center">
+<div class="redes mt-3">
+  <div class="Redes2 d-flex justify-content-center icons">
+    <a href="error404.html"
+      ><i class="fa-brands fa-square-facebook fa-3x mx-2"></i
+    ></a>
+    <a href="error404.html"
+      ><i class="fa-brands fa-square-x-twitter fa-3x"></i
+    ></a>
+    <a href="error404.html"
+      ><i class="fa-brands fa-square-instagram fa-3x mx-2"></i
+    ></a>
+    <a href="error404.html"
+      ><i class="fa-brands fa-square-whatsapp fa-3x"></i
+    ></a>
+  </div>
+
+  <h5 class="text-center mt-3">
+    &copy; 2024 RollingsGame. Todos los derechos reservados.
+  </h5>
+</div>
+</div>
+
+<div class="col-12 col-md-6 col-lg-4 d-flex justify-content-center">
+<div class="informacion-pag">
+  <div
+    class="informacion-pag text-center mt-4 mb-3 listNone"
+    
+  >
+    <li><a href="./paginaPrincipal.html">Inicio</a></li>
+    <li><a href="sobreNosotros.html">Sobre nosotros</a></li>
+    <li><a href="contacto.html">Contacto</a></li>
+    <li>
+      <a href="error404.html"
+        >Politicas de privacidad</a
+      >
+    </li>
+  </div>
+</div>
+</div>`;
+
 const tablaJuegos = document.getElementById("idTablaJuegos");
 const juegos = JSON.parse(localStorage.getItem("catalogoDeJuegos")) || [];
 const nuevoJuego = document.getElementById("idBotonNuevoJuego");
@@ -291,8 +453,12 @@ tablaJuegos.innerHTML = juegos
 function nuevoValorDeDestacado(valorActual) {
   const juegos = JSON.parse(localStorage.getItem("catalogoDeJuegos")) || [];
   if (valorActual) {
+    //habilitar botones de eliminar
     return false;
   } else {
+    //sacar boton de borrar
+    /*     const miElemento = document.getElementById("miId");
+    miElemento.classList.add("d-none"); */
     return true;
   }
 }
@@ -344,6 +510,7 @@ function eliminacionFisica(id) {
   const juegos = JSON.parse(localStorage.getItem("catalogoDeJuegos")) || [];
   const index = juegos.findIndex((juegaso) => juegaso.id === id);
   if (index !== -1) {
+    //Si es destacado entonces no borrar y mostrar alert que te saque
     juegos.splice(index, 1); // Eliminar el juego del array
     localStorage.setItem("catalogoDeJuegos", JSON.stringify(juegos));
     location.reload();
@@ -591,3 +758,24 @@ const validacionJuegoEditado = (event, identificadorDeJuego) => {
   localStorage.setItem("catalogoDeJuegos", JSON.stringify(juegos));
   location.reload();
 };
+
+function cerrarSesion() {
+  const usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
+
+  const userLogin = usuarios.find((u) => u.id && u.login === true);
+
+  if (userLogin) {
+    userLogin.login = false;
+    localStorage.setItem("usuarios", JSON.stringify(usuarios));
+  }
+  setTimeout(() => {
+    window.location.href = "/index.html";
+  }, 1000);
+}
+/* } else {
+  alert("No autorizado, por favor inicie sesion");
+  setTimeout(() => {
+    window.location.href = "./login.html";
+  }, 1000);
+}
+ */
