cc5e2240f897b0900a967ff2810ae2b3b5adf420
Isas321 <javier.isas321@gmail.com>
2024-03-04 23:22:00 -0300

    edicion con precarga de datos exitosa

--- js/adminJuegos.js
+++ js/adminJuegos.js
@@ -178,7 +178,7 @@ nuevoJuego.innerHTML = `
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title w-100 text-center">Nuevo Juego</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        <button type="button"  class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
         <form id="formularioNuevoJuego">
@@ -297,9 +297,11 @@ tablaJuegos.innerHTML = juegos
 
           <!-- EDITAR JUEGO -->
           <!-- Button trigger modal -->
-          <button type="button" class="btn btn-warning m-1" data-bs-toggle="modal" data-bs-target="#exampleModalEditar-${
+          <button type="button" class="btn btn-warning m-1" onclick="ubicaJuegoParaEditar(${
             juego.id
-          }">
+          })" data-bs-toggle="modal" data-bs-target="#exampleModalEditar-${
+      juego.id
+    }">
           <i class="fas fa-edit"></i>
           </button>
           
@@ -319,14 +321,14 @@ tablaJuegos.innerHTML = juegos
             <form id="formularioNuevoJuego">
               <div class="mb-1">
                 <label for="exampleInputEmail1" class="form-label">Nombre</label>
-                <input type="text" class="form-control" id="idInputTituloEdit" name="user" value="${
+                <input type="text" class="form-control" id="idInputTituloEdit" name="titulo" value="${
                   juego.titulo
-                }" aria-describedby="emailHelp">
+                }" onChange="cargaJuegoEditado(event)" aria-describedby="emailHelp">
               </div>
     
               <div class="mb-1">
                 <label for="disabledSelect" class="form-label">Categoria</label>
-                <select id="idSeleccionCategoriaEdit" class="form-select">
+                <select id="idSeleccionCategoriaEdit" class="form-select" onChange="cargaJuegoEditado(event)" name="categoria">
                   <option value="">Seleccionar categoria</option>
                   <option value="Aventura" ${
                     juego.categoria === "Aventura" ? "selected" : ""
@@ -345,57 +347,49 @@ tablaJuegos.innerHTML = juegos
     
                     <div class="mb-1">
                     <label for="exampleFormControlTextarea1" class="form-label">Descripcion</label>
-                    <textarea class="form-control" id="idDescripcionEdit" rows="2" value="">${
+                    <textarea class="form-control" id="idDescripcionEdit" rows="2" onChange="cargaJuegoEditado(event)" name="descripcion">${
                       juego.descripcion
                     }</textarea>   
                   </div>
     
                   <div class="mb-1">
                   <label for="exampleFormControlTextarea1" class="form-label">Requisitos de sistema Minimos</label>
-                  <textarea class="form-control" id="idRequisitosMinimosEdit" rows="2">${JSON.stringify(
+                  <textarea class="form-control" id="idRequisitosMinimosEdit" onChange="cargaJuegoEditado(event)" name="requisitos.minimos" rows="2">${JSON.stringify(
                     juego.requisitos.minimos
                   )}</textarea>   
                 </div>
     
                 <div class="mb-1">
                 <label for="exampleFormControlTextarea1" class="form-label">Requisitos de sistema recomendados</label>
-                <textarea class="form-control" id="idRequisitosRecomendadosEdit" rows="2">${JSON.stringify(
+                <textarea class="form-control" id="idRequisitosRecomendadosEdit" onChange="cargaJuegoEditado(event)" name="requisitos.recomendados" rows="2">${JSON.stringify(
                   juego.requisitos.recomendados
                 )}</textarea>   
               </div>
     
-                    <div class="mb-1">
-                      <label for="disabledSelect" class="form-label">Publicar</label>
-                      <select id="idPublicarEdit" class="form-select">
-                        <option value="Si" selected>Si</option>
-                        <option value="No">No</option>
-                      </select>
-                    </div>
+
     
                     <div class="mb-1">
                     <label for="exampleInputPassword1" class="form-label">Link de imagen</label>
-                    <input type="text" class="form-control" id="idInputLinkImagenEdit" name="link" value="${
+                    <input type="text" class="form-control" id="idInputLinkImagenEdit" onChange="cargaJuegoEditado(event)" name="imagen" value="${
                       juego.imagen
                     }">
                   </div>
     
                     <div class="mb-1">
                       <label for="exampleInputPassword1" class="form-label">Link trailer de juego</label>
-                      <input type="text" class="form-control" id="idInputLinkEdit" name="link" value="${
+                      <input type="text" class="form-control" id="idInputLinkEdit" name="url" onChange="cargaJuegoEditado(event)" value="${
                         juego.url
                       }">
                     </div>
     
                     <div class="mb-2">
                     <label for="exampleInputEmail1" class="form-label">Precio</label>
-                    <input type="number" step="any" class="form-control" id="idInputPrecioEdit" name="precio" value="${
+                    <input type="number" step="any" class="form-control" id="idInputPrecioEdit" name="precio" onChange="cargaJuegoEditado(event)" value="${
                       juego.precio
                     }" aria-describedby="emailHelp" >
                   </div>
                     
-                  <button type="button" class="btn btn-primary" id="idBotonGuardar" onclick="validacionJuegoEditado(event,${
-                    juego.id
-                  })">Guardar Cambios</button>
+                  <button type="button" class="btn btn-primary" id="idBotonGuardar" onclick="guardaJuegoEditado(event)" >Guardar Cambios</button>
                   </form>
                 </div>
               </div>
@@ -449,6 +443,98 @@ tablaJuegos.innerHTML = juegos
   )
   .join("");
 
+//  Editar juego
+
+//Validaciones colores de input para juego nuevo y editar
+function colorInput(idInput) {
+  document.getElementById(idInput).addEventListener("input", function () {
+    var valorInput = this.value;
+    if (valorInput.trim() === "") {
+      this.classList.add("is-invalid");
+    } else {
+      this.classList.remove("is-invalid");
+    }
+  });
+}
+
+let juegoOriginal = {};
+let editado = {};
+function ubicaJuegoParaEditar(id) {
+  juegoOriginal = juegos.find((item) => item.id === id);
+  console.log("busca ubicacion:", juegoOriginal.titulo);
+}
+
+const cargaJuegoEditado = (evento) => {
+  juegoOriginal = {
+    ...juegoOriginal,
+    [evento.target.name]: evento.target.value,
+  };
+};
+
+const guardaJuegoEditado = (event) => {
+  colorInput("idInputTituloEdit");
+  colorInput("idSeleccionCategoriaEdit");
+  colorInput("idDescripcionEdit");
+  //colorInput("idRequisitosMinimosEdit");
+  //colorInput("idRequisitosRecomendadosEdit");
+  colorInput("idInputLinkImagenEdit");
+  colorInput("idInputLinkEdit");
+  colorInput("idInputPrecioEdit");
+  if (juegoOriginal.titulo.trim() === "") {
+    alert("Por favor, ingresa un título para el juego.");
+    document.getElementById("idInputTituloEdit").classList.add("is-invalid");
+    return;
+  }
+  if (juegoOriginal.categoria.trim() === "") {
+    alert("Por favor, seleccione una categoría para el juego.");
+    document
+      .getElementById("idSeleccionCategoriaEdit")
+      .classList.add("is-invalid");
+    return;
+  }
+  if (juegoOriginal.descripcion.trim() === "") {
+    alert("Por favor, ingrese descripcion de juego");
+    document.getElementById("idDescripcionEdit").classList.add("is-invalid");
+    return;
+  }
+  /*   if (juegoOriginal.requisitoMinimo.trim() === "") {
+    alert("Por favor, ingrese requisito de sistema minimo para el juego");
+    document
+      .getElementById("idRequisitosMinimosEdit")
+      .classList.add("is-invalid");
+    return;
+  }
+  if (juegoOriginal.requisitoRecomendado.trim() === "") {
+    alert("Por favor, ingrese requisito de sistema recomendado para el juego");
+    document
+      .getElementById("idRequisitosRecomendadosEdit")
+      .classList.add("is-invalid");
+    return;
+  } */
+
+  if (juegoOriginal.imagen.trim() === "") {
+    alert("Por favor, ingrese link de la imagen del juego");
+    document
+      .getElementById("idInputLinkImagenEdit")
+      .classList.add("is-invalid");
+    return;
+  }
+  if (juegoOriginal.url.trim() === "") {
+    alert("Por favor, ingrese link del trailer del juego");
+    document.getElementById("idInputLinkEdit").classList.add("is-invalid");
+    return;
+  }
+  if (juegoOriginal.precio.toString().trim() === "") {
+    alert("Por favor, ingrese precio del juego");
+    document.getElementById("idInputPrecioEdit").classList.add("is-invalid");
+    return;
+  }
+  const index = juegos.findIndex((item) => item.id === juegoOriginal.id);
+  juegos[index] = juegoOriginal;
+  localStorage.setItem("catalogoDeJuegos", JSON.stringify(juegos));
+  location.reload();
+};
+
 // Boton de Destacado OK
 function nuevoValorDeDestacado(valorActual) {
   const juegos = JSON.parse(localStorage.getItem("catalogoDeJuegos")) || [];
@@ -474,14 +560,20 @@ function cambiarDestacado(id) {
     } else {
       if (juegosDestacados.length == 1) {
         if (juegosDestacados[0].id != id) {
-          const indexViejo = juegos.findIndex(
-            (juego) => juego.destacado === true
-          );
-          console.log(indexViejo);
-          console.log(juegosDestacados[0].id);
-          console.log(juegos[index].id);
-          juegos[indexViejo].destacado = false;
-          juegos[index].destacado = true;
+          if (juegos[index].publicado === true) {
+            const indexViejo = juegos.findIndex(
+              (juego) => juego.destacado === true
+            );
+            console.log(indexViejo);
+            console.log(juegosDestacados[0].id);
+            console.log(juegos[index].id);
+            juegos[indexViejo].destacado = false;
+            juegos[index].destacado = true;
+          } else {
+            alert("El juego no esta publicado, no es posible destacar");
+          }
+        } else {
+          alert("Debe tener un juego destacado");
         }
       } else {
         nuevoValor = nuevoValorDeDestacado(juegos[index].destacado);
@@ -508,11 +600,15 @@ function colorBotonDestacar(id) {
 
 function eliminacionFisica(id) {
   const juegos = JSON.parse(localStorage.getItem("catalogoDeJuegos")) || [];
-  const index = juegos.findIndex((juegaso) => juegaso.id === id);
+  let index = juegos.findIndex((juegaso) => juegaso.id === id);
   if (index !== -1) {
     //Si es destacado entonces no borrar y mostrar alert que te saque
-    juegos.splice(index, 1); // Eliminar el juego del array
-    localStorage.setItem("catalogoDeJuegos", JSON.stringify(juegos));
+    if (juegos[index].destacado === true) {
+      alert("El juego esta como destacado, no es posible eliminar");
+    } else {
+      juegos.splice(index, 1); // Eliminar el juego del array
+      localStorage.setItem("catalogoDeJuegos", JSON.stringify(juegos));
+    }
     location.reload();
   }
 }
@@ -520,10 +616,16 @@ function eliminacionFisica(id) {
 //Logica para boton de publicar OK
 function estadoDePublicacion(estado, id) {
   if (estado) {
-    despublicar(id);
+    const indexJuego = juegos.findIndex((juego) => juego.id === id);
+    if (juegos[indexJuego].destacado === true) {
+      alert("El juego esta como destacado, no es posible despublicar");
+    } else {
+      despublicar(id);
+    }
   } else {
     publicar(id);
   }
+  location.reload();
 }
 
 function publicar(id) {
@@ -542,18 +644,6 @@ function despublicar(id) {
   localStorage.setItem("catalogoDeJuegos", JSON.stringify(juegos));
   console.log("Despublicar");
   location.reload();
-}
-
-//Validaciones colores de input
-function colorInput(idInput) {
-  document.getElementById(idInput).addEventListener("input", function () {
-    var valorInput = this.value;
-    if (valorInput.trim() === "") {
-      this.classList.add("is-invalid");
-    } else {
-      this.classList.remove("is-invalid");
-    }
-  });
 }
 
 //Validaciones para nuevo Juego
@@ -632,30 +722,49 @@ const validacionJuego = (event) => {
     estadoDeJuego = false;
   }
 
-  let nuevoJuego = {
-    id: idNuevoJuego,
-    titulo: nombreJuego,
-    descripcion: descripcionJuego,
-    categoria: categoriaJuego,
-    precio: precioJuego,
-    requisitos: {
-      minimos: requisitoMinimo,
-      recomendados: requisitoRecomendado,
-    },
-    imagen: linkImagenJuego,
-    url: linkJuego,
-    publicado: estadoDeJuego,
-    destacado: false,
-  };
+  let nuevoJuego;
+  if (juegos.length === 0) {
+    nuevoJuego = {
+      id: idNuevoJuego,
+      titulo: nombreJuego,
+      descripcion: descripcionJuego,
+      categoria: categoriaJuego,
+      precio: precioJuego,
+      requisitos: {
+        minimos: requisitoMinimo,
+        recomendados: requisitoRecomendado,
+      },
+      imagen: linkImagenJuego,
+      url: linkJuego,
+      publicado: estadoDeJuego,
+      destacado: true,
+    };
+  } else {
+    nuevoJuego = {
+      id: idNuevoJuego,
+      titulo: nombreJuego,
+      descripcion: descripcionJuego,
+      categoria: categoriaJuego,
+      precio: precioJuego,
+      requisitos: {
+        minimos: requisitoMinimo,
+        recomendados: requisitoRecomendado,
+      },
+      imagen: linkImagenJuego,
+      url: linkJuego,
+      publicado: estadoDeJuego,
+      destacado: false,
+    };
+  }
   juegos.push(nuevoJuego);
   localStorage.setItem("catalogoDeJuegos", JSON.stringify(juegos));
-  console.log("Nuevo juego agregado:", nuevoJuego);
   location.reload();
 };
 
-//  Validacion para Editar Juego
+/* //  Validacion para Editar Juego
 const validacionJuegoEditado = (event, identificadorDeJuego) => {
   event.preventDefault();
+  const juegos = JSON.parse(localStorage.getItem("catalogoDeJuegos")) || [];
   colorInput("idInputTituloEdit");
   colorInput("idSeleccionCategoriaEdit");
   colorInput("idDescripcionEdit");
@@ -675,7 +784,6 @@ const validacionJuegoEditado = (event, identificadorDeJuego) => {
   let requisitoRecomendadoE = document.getElementById(
     "idRequisitosRecomendadosEdit"
   ).value;
-  let publicarJuegoE = document.getElementById("idPublicarEdit").value;
   let linkImagenJuegoE = document.getElementById("idInputLinkImagenEdit").value;
   let linkJuegoE = document.getElementById("idInputLinkEdit").value;
   let precioJuegoE = document.getElementById("idInputPrecioEdit").value;
@@ -730,38 +838,36 @@ const validacionJuegoEditado = (event, identificadorDeJuego) => {
     return;
   }
 
-  let estadoDeJuegoE;
-  if (publicarJuegoE === "Si") {
-    estadoDeJuegoE = true;
-  } else {
-    estadoDeJuegoE = false;
-  }
-
+  //Encuestro la posicion del juego
   const indexJuegoEditar = juegos.findIndex(
     (game) => game.id === identificadorDeJuego
   );
+
   juegoEditar = juegos[indexJuegoEditar];
 
-  juegoEditar.id = identificadorDeJuego;
-  juegoEditar.titulo = nombreJuegoE;
+  juegoEditar, (juegoEditar.titulo = nombreJuegoE);
   juegoEditar.descripcion = descripcionJuegoE;
   juegoEditar.categoria = categoriaJuegoE;
   juegoEditar.precio = precioJuegoE;
+  juegoEditar.requisitos = {}; // Asegúrate de que requisitos sea un objeto
   juegoEditar.requisitos.minimos = requisitoMinimoE;
   juegoEditar.requisitos.recomendados = requisitoRecomendadoE;
   juegoEditar.imagen = linkImagenJuegoE;
   juegoEditar.url = linkJuegoE;
-  juegoEditar.publicado = estadoDeJuegoE;
-  juegoEditar.destacado = false;
+
+  //juegoEditar = juegos[indexJuegoEditar];
+
+  console.log("juego editar", juegoEditar);
 
   juegos[indexJuegoEditar] = juegoEditar;
-  localStorage.setItem("catalogoDeJuegos", JSON.stringify(juegos));
-  location.reload();
+  // localStorage.setItem("catalogoDeJuegos", JSON.stringify(juegos));
+  //console.log("Editado");
+  //location.reload();
 };
+ */
 
 function cerrarSesion() {
   const usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
-
   const userLogin = usuarios.find((u) => u.id && u.login === true);
 
   if (userLogin) {
@@ -772,10 +878,3 @@ function cerrarSesion() {
     window.location.href = "/index.html";
   }, 1000);
 }
-/* } else {
-  alert("No autorizado, por favor inicie sesion");
-  setTimeout(() => {
-    window.location.href = "./login.html";
-  }, 1000);
-}
- */
